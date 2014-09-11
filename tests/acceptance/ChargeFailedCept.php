a5550555619616f9dc10b3166a71ebf41d250fd4
tomykaira <tomykaira@gmail.com>
2014-09-11 12:28:28 +0900

    Test card error

--- tests/acceptance/ChargeFailedCept.php
+++ tests/acceptance/ChargeFailedCept.php
@@ -1,3 +1,33 @@
 <?php
 $I = new AcceptanceTester($scenario);
-$I->wantTo('perform actions and see result');
+$I->wantTo('see descriptive error if my card is declined');
+
+$I->amOnPage('/');
+$I->fillField('amount', '800');
+
+$I->waitForElement('#WP_checkoutBox', 5);
+$I->executeJS('WebPay.testMode = true');
+
+$I->click('カードで支払う');
+$I->fillField('#WP_cardNumber', '4242 4242 4242 4242');
+$I->selectOption('#WP_expMonth', '12');
+$I->selectOption('#WP_expYear', '19');
+$I->fillField('#WP_name', 'TEST TEST');
+$I->fillField('#WP_cvc', '123');
+
+// エラーレスポンスを返す
+$I->pushMockResponse(402, [
+    'error' => [
+        'message' => 'このカードでは取引をする事が出来ません。利用出来ない理由をご契約中のカード会社へお問い合わせるか、他のカードをご利用ください。',
+        'caused_by' => 'buyer',
+        'type' => 'card_error',
+        'code' => 'card_declined'
+    ]
+]);
+
+$I->click('#WP_sendButton');
+
+$I->see('Status is:402');
+$I->see('Type is:card_error');
+$I->see('Code is:card_declined');
+$I->see('Message is:このカードでは取引をする事が出来ません。利用出来ない理由をご契約中のカード会社へお問い合わせるか、他のカードをご利用ください。');
