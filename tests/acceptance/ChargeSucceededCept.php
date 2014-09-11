f682b6e03b5f7cdbb77e3d62d7cd2b76f19f7d06
tomykaira <tomykaira@gmail.com>
2014-09-11 12:07:23 +0900

    Add dummy response

--- tests/acceptance/ChargeSucceededCept.php
+++ tests/acceptance/ChargeSucceededCept.php
@@ -17,6 +17,38 @@ $I->selectOption('#WP_expMonth', '12');
 $I->selectOption('#WP_expYear', '19');
 $I->fillField('#WP_name', 'TEST TEST');
 $I->fillField('#WP_cvc', '123');
+
+// 次の画面遷移で呼ばれるAPIのモック
+$I->pushMockResponse(201, [
+    'id' => 'ch_abcdefghijklmno',
+    'object' => 'charge',
+    'livemode' => false,
+    'currency' => 'jpy',
+    'description' => 'PHP からのアイテムの購入',
+    'amount' => 800,
+    'amount_refunded' => 0,
+    'customer' => null,
+    'recursion' => null,
+    'created' => time(),
+    'paid' => true,
+    'refunded' => false,
+    'failure_message' => null,
+    'card' => [
+        'object' => 'card',
+        'exp_year' => 2019,
+        'exp_month' => 12,
+        'fingerprint' => '215b5b2fe460809b8bb90bae6eeac0e0e0987bd7',
+        'name' => 'TEST TEST',
+        'country' => 'JP',
+        'type' => 'Visa',
+        'cvc_check' => 'pass',
+        'last4' => '4242'
+    ],
+    'captured' => true,
+    'expire_time' => null,
+    'fees'
+]);
+
 $I->click('#WP_sendButton');
 
 $I->see('お支払いありがとうございました');
